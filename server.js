f14af634f6e6d7607613b7336d942cbb11b8c01c
romeoscript <romeobourne211@gmail.com>
2024-03-26 11:36:20 +0100

    chatty

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const swaggerOptions = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "NFT API",
+      title: "Chattie API",
       version: "1.0.0",
       description: "A simple Express NFT API",
     },
